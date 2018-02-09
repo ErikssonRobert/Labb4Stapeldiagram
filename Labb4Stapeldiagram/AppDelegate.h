26d36ab7a985dba8fcfc5025e2e97c8750c5ecf8
Robert <roberteriksson12@gmail.com>
2018-02-09 17:43:39 +0100

    Initial Commit

---
+++ Labb4Stapeldiagram/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Labb4Stapeldiagram
+//
+//  Created by lösen är 0000 on 2018-02-09.
+//  Copyright © 2018 Robert Eriksson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Labb4Stapeldiagram/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Labb4Stapeldiagram
+//
+//  Created by lösen är 0000 on 2018-02-09.
+//  Copyright © 2018 Robert Eriksson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
